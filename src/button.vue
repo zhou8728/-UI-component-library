ff100ba15dc1795b7d84b559f25041b291234205
zhoudc <1292940661@qq.com>
2018-08-03 11:29:36 +0800

    验证iconPosition为left货right中的一个 设置该属性为其他值将报错

--- src/button.vue
+++ src/button.vue
@@ -12,8 +12,19 @@
 <script>
     export default {
 
-        props: ['icon', 'iconPosition']
+        // props: ['icon', 'iconPosition']
+        props:{
+            icon:{},
+            iconPosition:{
+                type: String,
+                default:'left',
+                validator(value){
+                    console.log(value)
+                    return !(value !== 'left' && value !== 'right');
+                }
 
+            }
+        }
 
     }
 </script>
