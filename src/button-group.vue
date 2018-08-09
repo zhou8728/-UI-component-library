3be1fd5b7772aea60b31bf523728dc4b13a8bfab
zhoudc <1292940661@qq.com>
2018-08-09 23:34:34 +0800

    创建button-group

---
+++ src/button-group.vue
@@ -0,0 +1,40 @@
+<template>
+    <div class="g-button-group">
+        <slot></slot>
+    </div>
+
+</template>
+
+<script>
+    export default {
+        name: "button-group.vue"
+    }
+</script>
+
+<style lang="scss" scoped>
+    .g-button-group{
+        display: inline-flex;
+        vertical-align: middle;
+        >.g-button{
+            border-radius: 0;
+            margin-left:-1px;
+            /*&:not(:first-child){*/
+                /*border-left:none;*/
+            /*}*/
+            &:first-child{
+                border-top-left-radius: var(--border-radius);
+                border-bottom-left-radius: var(--border-radius);
+            }
+            &:last-child{
+                border-top-right-radius:  var(--border-radius);
+                border-bottom-right-radius: var(--border-radius);
+            }
+            &:hover{
+                position:relative;
+                z-index: 1;
+            }
+
+        }
+    }
+
+</style>
