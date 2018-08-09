f1652b5e87224902b9f51869525d2a6f3cf447b9
zhoudc <1292940661@qq.com>
2018-08-09 21:57:06 +0800

    添加click事件

--- src/app.js
+++ src/app.js
@@ -9,85 +9,81 @@ Vue.component('g-icon',Icon)
 
 new Vue({
     el:'#app',
+    data:{
+        loading1:false,
+        loading2:false,
+        loading3:false
+    }
 })
 
 //单元测试
-import chai from 'chai'
-import spies from 'chai-spies'
-chai.use(spies)
-const expect=chai.expect
-{
-   const Constructor=Vue.extend(Button)
-    const  button =new Constructor({
-        propsData:{
-            icon:'settings'
-        }
-    })
-    button.$mount()
-    let useElement=button.$el.querySelector('use')
-
-    let href=useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#i-settings')
-
-
-}
-
-{
-    const Constructor=Vue.extend(Button)
-    const button =new Constructor({
-        propsData:{
-            icon:'settings',
-            loading:true
-        }
-    })
-
-    button.$mount()
-    let useElement=button.$el.querySelector('use')
-    let href=useElement.getAttribute('xlink:href')
-    expect(href).to.eq('#i-loading')
-}
+// import chai from 'chai'
+// import spies from 'chai-spies'
+// chai.use(spies)
+// const expect=chai.expect
+// console.log(chai)
+// {
+//    const Constructor=Vue.extend(Button)
+//     const  button =new Constructor({
+//         propsData:{
+//             icon:'settings'
+//         }
+//     })
+//     button.$mount()
+//     let useElement=button.$el.querySelector('use')
+//
+//     let href=useElement.getAttribute('xlink:href')
+//     expect(href).to.eq('#i-settings')
+//
+//
+// }
 
 
-{
-    const div=document.createElement('div')
-    document.body.appendChild(div)
-    const Constructor=Vue.extend(Button)
-    const button =new Constructor({
-        propsData:{
-            icon:'settings',
-            loading:true
-        }
-    })
 
-    button.$mount(div)
-    let svg=button.$el.querySelector('svg')
-    let {order}=window.getComputedStyle(svg)
-    expect(order).to.eq('2')
-    button.$el.remove()
-    button.$destroy()
-}
 
-//mock
-{
 
-    const Constructor=Vue.extend(Button)
-    const  vm=new Constructor({
-        propsData:{
-            icon:'settings',
 
-        }
-    })
 
-    vm.$mount()
-    let spy=chai.spy(function(){
+// {
+//     const Constructor=Vue.extend(Button)
+//     const button =new Constructor({
+//         propsData:{
+//             icon:'settings',
+//             loading:true
+//         }
+//     })
+//
+//     button.$mount()
+//     let useElement=button.$el.querySelector('use')
+//     let href=useElement.getAttribute('xlink:href')
+//     expect(href).to.eq('#i-settings')
+// }
 
-    })
-    vm.$on('click',function(){
-        expect(1).to.eq(1)
 
-    })
-    let button=vm.$el
-    button.click()
-    expect(spy).to.have.been.called()
 
-}
+
+//
+// {
+//     const div=document.createElement('div')
+//     document.body.appendChild(div)
+//     const Constructor=Vue.extend(Button)
+//     const button =new Constructor({
+//         propsData:{
+//             icon:'settings',
+//             loading:true
+//         }
+//     })
+//
+//     button.$mount(div)
+//     let svg=button.$el.querySelector('svg')
+//     let {order}=window.getComputedStyle(svg)
+//     expect(order).to.eq('1')
+//     button.$el.remove()
+//     button.$destroy()
+// }
+
+
+
+
+
+
--- src/button.vue
+++ src/button.vue
@@ -1,10 +1,12 @@
 <template>
-    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}">
+    <button class="g-button" :class="{[`icon-${iconPosition}`]:true}"
+    @click="$emit('click')">
+
         <!--<svg v-if="icon" class="icon">-->
             <!--<use :xlink:href="`#i-${icon}`"></use>-->
         <!--</svg>-->
-        <g-icon class="icon" v-if="icon" :name="icon"></g-icon>
-        <g-icon class="loading" name="loading"></g-icon>
+        <g-icon class="icon" v-if="icon&&!loading" :name="icon"></g-icon>
+        <g-icon class="loading" v-if="loading" name="loading"></g-icon>
         <div class="content">
             <slot></slot>
         </div>
@@ -17,6 +19,10 @@
         // props: ['icon', 'iconPosition']
         props:{
             icon:{},
+            loading:{
+                type:Boolean,
+                default:false
+            },
             iconPosition:{
                 type: String,
                 default:'left',
